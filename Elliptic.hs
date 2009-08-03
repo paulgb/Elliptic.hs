a938cf6a3972783f7787dfaef4f1074f9e3400a4
Paul Butler <paul@wildcard.(none)>
2009-08-03 00:11:02 -0700

    negation, subtraction, more complete addition

--- Elliptic.hs
+++ Elliptic.hs
@@ -4,13 +4,16 @@ module Elliptic where
 import Maybe
 
 -- (x,y) point or infinity
-data Point = Point Int Int | Infinity
+data Point = Point Int Int | Infinity | Debug Int Int Int Int
 
 instance Show Point where
   show Infinity = "inf"
   show (Point x y) =
     "("++(show x)++","
     ++(show y)++")"
+  show (Debug a b c d) =
+    (show a) ++ " " ++ (show b) ++ " " ++
+    (show c) ++ " " ++ (show d)
 
 -- a, b, m
 data EllipticCurve = Curve Int Int Int
@@ -33,6 +36,7 @@ points (Curve a b m) =
 
 -- modular inverse
 -- (mInverse x m) x `mod` m == 1
+-- this is a slow brute force algorithm, there are better
 modInverse :: Int -> Int -> Maybe Int
 modInverse x m =
   listToMaybe $ do
@@ -42,11 +46,36 @@ modInverse x m =
 
 -- somewhat inspired by this code:
 -- http://programmingpraxis.com/2009/07/31/elliptic-curve-factorization/2/
--- addPoints :: EllipticCurve -> Point -> Point -> Point
+
+addPoints :: EllipticCurve -> Point -> Point -> Point
+
+addPoints _ Infinity p2 = p2
+addPoints _ p1 Infinity = p1
+
+-- todo: factor out duplicate code
+-- todo: check that point lies on curve
+addPoints (Curve a b m) (Point x y) (Point u v)
+    | (x, y) == (u, v) =
+        if ((y + v) `mod` m) == 0 then
+            Infinity
+        else
+            let n = (3 * x * x) + a in
+            let (Just d) = (modInverse (2 * y) m) in
+            let x' = ((n*d)^2 - (x + u)) `mod` m in
+            let y' = ((n*d*(x-x'))-y) `mod` m in
+            Point x' y'
+
 addPoints (Curve a b m) (Point x y) (Point u v) =
   let (Just d) = modInverse (u - x) m in
   let n = v - y in
   let x' = ((n*d)^2 - (x + u)) `mod` m in
   let y' = ((n*d*(x-x'))-y) `mod` m in
   Point x' y'
+
+negatePoint :: EllipticCurve -> Point -> Point
+negatePoint (Curve _ _ m) (Point x y) = Point x ((-y) `mod` m)
+negatePoint _ Infinity = Infinity
+
+subtractPoints :: EllipticCurve -> Point -> Point -> Point
+subtractPoints c p1 p2 = addPoints c p1 (negatePoint c p2)
 
